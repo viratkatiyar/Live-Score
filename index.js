e53f8701c625e83ac4cfd22e8c35e033f3504d52
viratkatiyar <viratkatiyar21@gmail.com>
2026-08-29 17:14:38 +0530

    completed

---
+++ index.js
@@ -0,0 +1,33 @@
+let team1Score = document.getElementById("team-1")
+let count1 = 0
+function incrementTeam11(){
+    count1 += 1
+    team1Score.textContent = count1
+}
+function incrementTeam12(){
+    count1 += 2
+    team1Score.textContent = count1
+}
+function incrementTeam13(){
+    count1 += 3
+    team1Score.textContent = count1
+}
+
+let team2Score = document.getElementById("team-2")
+
+let count2 = 0
+
+function incrementTeam21() {
+    count2 += 1
+    team2Score.textContent = count2
+}
+
+function incrementTeam22() {
+    count2 += 2
+    team2Score.textContent = count2
+}
+
+function incrementTeam23() {
+    count2 += 3
+    team2Score.textContent = count2
+}
